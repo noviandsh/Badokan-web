de4682604d8973dc7572a6dc0d9305ea3b89ec59
Novian Dwi Syahrizal <noviandsh@gmail.com>
2021-07-07 17:05:52 +0700

    integration test added

--- src/scripts/views/templates/like-button-template.js
+++ src/scripts/views/templates/like-button-template.js
@@ -1,11 +1,11 @@
 const createLikeButtonTemplate = () => `
-  <button aria-label="like this movie" id="likeButton" class="like">
+  <button aria-label="like this restaurant" id="likeButton" class="like">
     <i class="material-icons" aria-hidden="true">favorite_border</i>
   </button>
 `;
 
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this movie" id="likeButton" class="like">
+  <button aria-label="unlike this restaurant" id="likeButton" class="like">
     <i class="material-icons" aria-hidden="true">favorite</i>
   </button>
 `;
